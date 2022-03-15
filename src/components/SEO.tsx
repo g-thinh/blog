e0efdb10ae8bae693b202865f7f63ca1f4f8e836
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2022-03-14 22:17:33 -0400

    added keywords, author and published date tag, fixed url tag, stringified blog tags

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -12,11 +12,20 @@ export default function SEO({ meta }: SEOProps): JSX.Element {
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <title>Gia Thinh Nguyen - {meta.title}</title>
+      <meta name="author" content="Gia Thinh Nguyen" />
+      {meta.tags && <meta name="keywords" content={meta.tags?.toString()} />}
+      {meta.publishedDate && (
+        <meta
+          name="publish_date"
+          property="og:publish_date"
+          content={meta.publishedDate?.toString()}
+        />
+      )}
       <meta name="description" content={meta.description} />
       <meta property="og:title" content={meta.title} />
       <meta property="og:description" content={meta.description} />
       <meta property="og:image" content={meta.imageUrl} />
-      <meta property="og:url" content={process.env.baseUrl + router.pathname} />
+      <meta property="og:url" content={process.env.baseUrl + router.asPath} />
       <meta name="twitter:title" content={meta.title} />
       <meta name="twitter:description" content={meta.description} />
       <meta name="twitter:image" content={meta.imageUrl} />
