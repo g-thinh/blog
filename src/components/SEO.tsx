8e6e017a386dda737eb16b3165ee25408c182378
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2022-03-14 22:47:57 -0400

    updated publish date for article

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,6 +1,15 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { MDXFrontmatter } from "utils/mdxUtils";
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(advancedFormat);
+dayjs.extend(timezone);
+dayjs.extend(utc);
+dayjs.tz.setDefault("Eastern Daylight Time");
 
 type SEOProps = {
   meta: MDXFrontmatter;
@@ -13,12 +22,12 @@ export default function SEO({ meta }: SEOProps): JSX.Element {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <title>Gia Thinh Nguyen - {meta.title}</title>
       <meta name="author" content="Gia Thinh Nguyen" />
+      <meta property="og:type" content="article" />
       {meta.tags && <meta name="keywords" content={meta.tags?.toString()} />}
       {meta.publishedDate && (
         <meta
-          name="publish_date"
-          property="og:publish_date"
-          content={meta.publishedDate?.toString()}
+          property="article:published_time"
+          content={dayjs(meta.publishedDate).format("YYYY-MM-DDTHH:mm:ssZZ")}
         />
       )}
       <meta name="description" content={meta.description} />
